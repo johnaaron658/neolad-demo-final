628b7e212dc4adc515292dc4d05398e73a3c7128
John Aaron Alcoseba <johnaaron658@gmail.com>
2020-07-14 12:11:43 +0800

    initiial commit

---
+++ src/main/java/com/example/neoloaddemofinal/NeoloadDemoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoloaddemofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoloadDemoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoloadDemoFinalApplication.class, args);
+	}
+
+}
